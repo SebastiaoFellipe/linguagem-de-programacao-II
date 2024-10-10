09cabe039fdaa450a45c81ffbf46ec731013404a
SebastiaoFellipe <sebaslopes369@gmail.com>
2024-10-10 10:59:34 -0300

    Adicionando lista de exercicios 01

---
+++ lista-de-exercicios-01/src/lista01.java
@@ -0,0 +1,133 @@
+import java.util.Scanner;
+
+public class lista01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // System.out.println("\nQUESTÃO 1");
+        // float base = 7.5f, altura = 12.3f;
+        // float perimetro = 2*base+2*altura;
+        // System.out.println("Área do retângulo: "+(base*altura));
+        // System.out.println("Perimêtro do retângulo: "+perimetro);
+
+        // System.out.println("\nQUESTÃO 2");
+        // float lado = 8f;
+        // System.out.println("Área do quadrado: "+(lado*lado));
+        // System.out.println("Perimêtro do quadrado: "+(lado*4));        
+
+        // System.out.println("\nQUESTÃO 3");
+        // float raio = 2.8f;
+        // System.out.println("Área do circulo: "+(Math.PI*raio*raio));
+        // System.out.println("Perimêtro do circulo: "+(2*Math.PI*raio));
+
+        // System.out.println("\nQUESTÃO 4");
+        // float lado = 5.2f;
+        // System.out.println("Perimêtro do triângulo: "+(3*lado));
+
+        // System.out.println("\nQUESTÃO 5");
+        // int num1 = scanner.nextInt();
+        // int num2 = scanner.nextInt();
+        // int quociente = num1/num2;
+        // System.out.println("Quociente: "+quociente);
+        // System.out.println("Resto da divisão: "+(num1-(num2*quociente)));
+
+        // System.out.println("\nQUESTÃO 6");
+        // System.out.print("Digite sua idade em dias: ");
+        // int diasTotal = scanner.nextInt();
+        // int anos = diasTotal/365;
+        // int sobra = diasTotal-365*anos;
+        // int meses = sobra/30;
+        // int dias = sobra-30*meses;
+        // System.out.println("Você tem "+anos+" anos, "+meses+" meses e "+dias+ " dias");
+
+        // System.out.println("\nQUESTÃO 7");
+        // System.out.print("Digite a temperatura em Fahrenheit: ");
+        // float fahrenheit = scanner.nextInt();
+        // float celsius = 5*(fahrenheit-32)/9;
+        // System.out.println("Temperatura em Celsius: "+celsius);
+
+        // System.out.println("\nQUESTÃO 8");
+        // double raio = 2.6;
+        // double altura = 22.1;
+        // double volume = Math.PI * Math.pow(raio, 2) * altura;
+        // System.out.println("O volume da lata de óleo é: "+volume);
+
+        // System.out.println("\nQUESTÃO 9");
+        // int num = -1;
+        // while (num < 0 || num >= 32) {
+        //     System.out.print("Digite um número inteiro positivo menor que 32: ");
+        //     num = scanner.nextInt();
+        // }
+        // int[] binario = new int[5];
+        // int i = 0, n = num;
+        // while (n > 0) {
+        //     binario[i] = n % 2;
+        //     n = n / 2;
+        //     i++;
+        // }
+        // System.out.print("A representação binária de " + num + " é: ");
+        // for (int j = 4; j >= 0; j--) {
+        //     System.out.print(binario[j]);
+        // }
+
+        // System.out.println("\nQUESTÃO 10");
+        // float nota1Bimestre1, nota2Bimestre1, nota1Bimestre2, nota2Bimestre2;
+        // System.out.print("Digite a nota 1 do 1º bimestre: ");
+        // nota1Bimestre1 = scanner.nextFloat();
+        // System.out.print("Digite a nota 2 do 1º bimestre: ");
+        // nota2Bimestre1 = scanner.nextFloat();
+        // System.out.print("Digite a nota 1 do 2º bimestre: ");
+        // nota1Bimestre2 = scanner.nextFloat();
+        // System.out.print("Digite a nota 2 do 2º bimestre: ");
+        // nota2Bimestre2 = scanner.nextFloat();
+        // float media1 = (nota1Bimestre1 + nota2Bimestre1)/2;
+        // float media2 = (nota1Bimestre2 + nota2Bimestre2)/2;
+        // float mediaSemestral = (media1 + media2)/2;
+        // System.out.println("A média do aluno é: "+mediaSemestral);
+
+        // System.out.println("\nQUESTÃO 11");
+        // System.out.print("Digite a velocidade em m/s: ");
+        // float veloMS = scanner.nextFloat();
+        // float valoKMH = veloMS * 3.6f;
+        // System.out.println("A velocidade em km/h é: "+valoKMH);
+
+        // System.out.println("\nQUESTÃO 12");
+        // System.out.print("Digite o valor da resistência R1: ");
+        // float R1 = scanner.nextFloat();
+        // System.out.print("Digite o valor da resistência R2: ");
+        // float R2 = scanner.nextFloat();
+        // System.out.print("Digite o valor da resistência R3: ");
+        // float R3 = scanner.nextFloat();
+        // float Rp = 1/((1/R1)+(1/R2));
+        // float Rtotal = Rp + R3;
+        // System.out.println("A resistência equivalente do circuito é: "+Rtotal);
+
+        // System.out.println("\nQUESTÃO 13");
+        // System.out.print("Digite o tempo gasto da viagem em horas: ");
+        // float tempo = scanner.nextFloat();
+        // System.out.print("Digite a velocidade média em km/h: ");
+        // float velocidade = scanner.nextFloat();
+        // float distancia = tempo * velocidade;
+        // float litrosGastos = distancia / 12f;
+        // System.out.println("A quantidade de litros de combustível gastos na viagem é: "+litrosGastos+"L");
+
+        // System.out.println("\nQUESTÃO 14");
+        // System.out.print("Digite o valor de A: ");
+        // int A = scanner.nextInt();
+        // System.out.print("Digite o valor de B: ");
+        // int B = scanner.nextInt();
+        // System.out.println("A: "+(B-A+A));
+        // System.out.println("B: "+(A-B+B));
+
+        // System.out.println("\nQUESTÃO 15");
+        // float comprimento, largura, altura, volume;
+        // System.out.print("Digite o comprimento da caixa: ");
+        // comprimento = scanner.nextFloat();
+        // System.out.print("Digite a largura da caixa: ");
+        // largura = scanner.nextFloat();
+        // System.out.print("Digite a altura da caixa: ");
+        // altura = scanner.nextFloat();
+        // volume = comprimento*largura*altura;
+        // System.out.println("O volume da caixa é: "+volume);
+    }
+}
