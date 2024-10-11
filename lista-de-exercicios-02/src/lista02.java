fea1dde13a18e6bec10538113cf31ba21edd4e84
SebastiaoFellipe <sebaslopes369@gmail.com>
2024-10-11 15:42:00 -0300

    Adicionando lista de exercicios 02

---
+++ lista-de-exercicios-02/src/lista02.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+public class lista02 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        // System.out.println("\nQUESTÃO 01");
+        // System.out.print("Digite a 1º nota: ");
+        // double nota1 = scanner.nextInt();
+        // System.out.print("Digite a 2º nota: ");
+        // double nota2 = scanner.nextInt();
+        // System.out.print("Digite a 3º nota: ");
+        // double nota3 = scanner.nextInt();
+        // double media = (nota1+nota2+nota3)/3;
+        // if (media >= 6 && nota1>=4 && nota2>=4 && nota3>=4) {
+        //     System.out.println("Aprovado!");
+        // } else if (media<3) {
+        //     System.out.println("Reprovado.");
+        // } else {
+        //     System.out.println("Tem direito à realização de uma avaliação de reposição.");
+        // }
+
+        // System.out.println("\nQUESTÃO 02");
+        // System.out.print("Digite um caractere: ");
+        // char caractere = scanner.next().charAt(0);
+        // caractere = Character.toLowerCase(caractere);
+        // if (Character.isDigit(caractere)) {
+        //     System.out.println("O caractere é um número.");
+        // } else if (Character.isLetter(caractere)) {
+        //     if (caractere == 'a' ||
+        //     caractere == 'e' ||
+        //     caractere == 'i' ||
+        //     caractere == 'o' ||
+        //     caractere == 'u') {
+        //     System.out.println("O caractere é uma vogal.");
+        //     } else {
+        //         System.out.println("O caractere é uma consoante.");
+        //     }
+        // } else {
+        //     System.out.println("O caractere é um símbolo.");
+        // }
+
+        // System.out.println("\nQUESTÃO 03");
+        // System.out.print("Digite um ano: ");
+        // int ano = scanner.nextInt();
+        // if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+        //     System.out.println(ano+" é um ano bissexto.");
+        // } else {
+        //     System.out.println(ano+" não é um ano bissexto.");
+        // }
+
+        // System.out.println("\nQUESTÃO 04");
+        // String senha;
+        // do {
+        //     System.out.print("Digite a senha: ");
+        //     senha = scanner.nextLine();
+        //     if (!senha.equals("1234")) {
+        //     System.out.println("ACESSO NEGADO");
+        //     }
+        // } while (!senha.equals("1234"));
+        // System.out.println("ACESSO PERMITIDO");
+
+        // System.out.println("\nQUESTÃO 05");
+        // for (int i = 1; i <= 9; i++) {
+        //     for (int j = 1; j <= 10; j++) {
+        //         System.err.println(i+" x "+j+" = "+(i*j));
+        //     }
+        //     System.out.println();
+        // }
+
+        // System.out.println("\nQUESTÃO 06");
+        // System.out.print("Digite um número: ");
+        // int n = scanner.nextInt();
+        // double euler = 0.0;
+        // for (int i = 0; i < n; i++) {
+        //     int fatorial = 1;
+        //     for (int j = 1; j <= i; j++) {
+        //         fatorial *= j;
+        //     }
+        //     euler += 1.0/fatorial;
+        // }
+        // System.out.println("Resultado: "+euler);
+
+        // System.out.println("\nQUESTÃO 07");
+        // System.out.print("Digite um número: ");
+        // int n = scanner.nextInt();
+        // int fatorial = 1;
+        // for (int i = 1; i <= n; i++) {
+        //     fatorial *= i;
+        // }
+        // System.out.println("Fatorial: "+fatorial);
+
+        // System.out.println("\nQUESTÃO 08");
+        // System.out.print("Digite um número natural inteiro par: ");
+        // int num = scanner.nextInt();
+        // System.out.print("Os divisores são: ");
+        // int i = 1;
+        // while (i <= num) {
+        //     if (num % i == 0) {
+        //         System.out.print(i+" ");
+        //     }
+        //     i++;
+        // }
+    }
+}
